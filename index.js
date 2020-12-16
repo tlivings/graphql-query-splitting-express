b565946daf972205d5c831a19d859d6272411d0b
Trevor Livingston <tlivings@gmail.com>
2020-12-16 13:45:03 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const gql = require('graphql-tag');
+const { splitDocumentByUpstream } = require('./lib/graphql-utils');
+
+function graphqlRouter({
+  // function (operation: String, attribute: String): String
+  // responsible for mapping the root operation to a unique key
+  mapDestination, 
+  // async function (destinationName: String, document: GraphQLDocument) : { [key: String]: String }
+  // responsible for executing the document against the given destination key
+  callDestination 
+}) {
+  
+  return function (req, res, next) {
+    
+    const document = gql(req.body);
+    
+    const upstreamDocuments = splitDocumentByUpstream(mapDestination, document);
+    
+    const work = Object.entries(upstreamDocuments).map(([upstream, document]) => {
+      return callDestination(upstream, document);
+    });
+
+    Promise.all(work).then((results) => {
+      const merged = results.reduce((merge, result) => {
+        merge.data = Object.assign(merge.data, result.data);
+        
+        if (result.errors && result.errors.length) {
+          merge.errors.push(...result.errors);
+        }
+        
+        return merge;
+      }, { data: {}, errors: [] });
+      
+      req.graphqlRouting = {
+        result: merged,
+        originalQuery: req.body
+      };
+      
+      next();
+    }).catch(next);
+  };
+}
+
+module.exports = graphqlRouter;
---
+++ test/test-middleware.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const test = require('tape');
+const graphqlRouting = require('../index');
+
+test('middlewware', function (t) {
+  
+  const middleware = graphqlRouting({
+    mapDestination(operation, name) {
+      return name;
+    },
+    callDestination(upstream, document) {
+      return { data: { [upstream]: `${upstream}.processed` } };
+    }
+  });
+  
+  const req = {
+    body: 'query { foo, bar }'
+  }
+  
+  middleware(req, undefined, (error) => {
+    t.plan(4);
+    t.error(error);
+    t.ok(req.graphqlRouting && req.graphqlRouting.result);
+    
+    const { data } = req.graphqlRouting.result || {};
+    
+    t.equal(data.foo, 'foo.processed');
+    t.equal(data.bar, 'bar.processed');
+    
+    //console.log(JSON.stringify(req.graphqlRouting, null, 2));
+  });
+});
